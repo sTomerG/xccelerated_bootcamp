8909a510143232a7a4e83e922688839492c3f3f0
Tomer Gabay <tomergabay@Admins-MacBook-Pro.local>
2022-09-08 09:34:20 +0200

    finished fastapi assignment

---
+++ fastapi/README.md
@@ -0,0 +1,6 @@
+# Instructions
+
+`docker compose up`
+
+
+
--- fastapi/src/fastapi_xcc/app.py
+++ fastapi/src/fastapi_xcc/app.py
@@ -1,36 +1,38 @@
+import json
+
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from redis import Redis
 
-from fast_api_xcc.models import Person
+from fastapi_xcc.models import Person
 
-red: Redis = Redis(decode_responses=True)
+red: Redis = Redis(
+    host="docker.for.mac.localhost", port=6379, decode_responses=True
+)
 app: FastAPI = FastAPI()
-red.set("persons", )
 
 
 @app.get("/")
 def index():
-    return {"Hello from the index"}
+    return {"Hello from the index!"}
 
 
 @app.post("/names/", status_code=201)
 def post_names(person: Person):
-    dictio = {person.name: person}
-    red.hset('persons', mapping=dict(person))
+    red.hset("persons", person.name, person.json())
     return JSONResponse(f"Added {person.name} to database")
 
 
 @app.get("/names/", status_code=200)
 def get_all_names():
-    return JSONResponse(list(red.hget()))
+    return JSONResponse(list(red.hgetall("persons")))
 
 
 @app.get("/names/{name}", status_code=200)
 def get_name(name: str):
     try:
-        print(dict(persons[name]))
+        person = red.hget("persons", name)
     except KeyError:
         raise HTTPException(404, f"'{name}' not known in database")
 
-    return JSONResponse(dict(persons[name]))
+    return JSONResponse(json.loads(person))
